5f0f2d475fccb1cc0b3bc260a84a4e62e336b5e3
Steve Moyer <smoyer1@selesy.com>
2025-08-16 07:26:38 -0400

    docs: add Go package docs for buyer

--- doc.go
+++ doc.go
@@ -1,4 +1,15 @@
 // Package buyer provides a library of code that allows the standard
 // library's http.Client to pay for HTTP content and services using the
 // x402 protocol.
+//
+// It is anticipated that this software will commonly be used to allow
+// AI agents to pay for the services they need.  When allowing automated
+// payments on your behalf, care should be taken to limit your financial
+// exposure.
+//
+// Defaults
+//
+//   - If the WithClient option is not specified, the http.DefaultClient
+//     is used with the http.DefaultTransport.
+//   - If the WithLogger Option is not specified, a No-Op logger is used.
 package buyer
